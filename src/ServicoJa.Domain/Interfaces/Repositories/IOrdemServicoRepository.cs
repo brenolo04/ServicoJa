a319b0e142500aba10d1213bfb06ba08cf482d9f
Breno C. Fonseca <brenocaires04@gmail.com>
2026-04-21 21:44:55 -0300

    feat: implementado repositorio da ordem de servico

--- src/ServicoJa.Domain/Interfaces/Repositories/IOrdemServicoRepository.cs
+++ src/ServicoJa.Domain/Interfaces/Repositories/IOrdemServicoRepository.cs
@@ -5,8 +5,8 @@ namespace ServicoJa.Domain.Repositories;
 public interface IOrdemServicoRepository
 {
     Task CriarOrdemServicoAsync(OrdemServico ordemServico);
-    Task<IEnumerable<OrdemServico>?> ObterTodosServicosPrestadosAsync(long idPerfil);
-    Task<IEnumerable<OrdemServico>?> ObterTodosServicosSolicitadosAsync(long idPerfil);
+    Task<IEnumerable<OrdemServico>> ObterTodosServicosPrestadosAsync(long idPerfil);
+    Task<IEnumerable<OrdemServico>> ObterTodosServicosSolicitadosAsync(long idPerfil);
     Task<OrdemServico?> ObterOrdemServicoPorIdAsync(long id);
     Task SalvarAsync();
 }
---
+++ src/ServicoJa.Infra/Repositories/OrdemServicoRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServicoJa.Domain.Models;
+using ServicoJa.Domain.Repositories;
+using ServicoJa.Infra.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicoJa.Infra.Repositories;
+
+public class OrdemServicoRepository : IOrdemServicoRepository
+{
+    private readonly ServicoJaDbContext _context;
+
+    public OrdemServicoRepository(ServicoJaDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CriarOrdemServicoAsync(OrdemServico ordemServico)
+        => await _context.OrdemServicos.AddAsync(ordemServico);
+
+    public async Task<OrdemServico?> ObterOrdemServicoPorIdAsync(long id)
+        => await _context.OrdemServicos.FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task<IEnumerable<OrdemServico>> ObterTodosServicosPrestadosAsync(long idPerfil)
+        => await _context.OrdemServicos
+            .AsNoTracking()
+            .Where(x => x.IdPerfilPrestador == idPerfil)
+            .ToListAsync();
+
+    public async Task<IEnumerable<OrdemServico>> ObterTodosServicosSolicitadosAsync(long idPerfil)
+        => await _context.OrdemServicos
+            .AsNoTracking()
+            .Where(x => x.IdPerfilSolicitante == idPerfil)
+            .ToListAsync();
+
+    public async Task SalvarAsync()
+        => await _context.SaveChangesAsync();
+}
