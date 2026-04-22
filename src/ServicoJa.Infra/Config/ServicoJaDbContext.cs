44f694dca630ca58364b686878a4bf47813c3a3a
Breno C. Fonseca <brenocaires04@gmail.com>
2026-04-21 21:29:20 -0300

    feat: adicionado dbsets

--- src/ServicoJa.Infra/Config/ServicoJaDbContext.cs
+++ src/ServicoJa.Infra/Config/ServicoJaDbContext.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ServicoJa.Domain.Models;
 
 namespace ServicoJa.Infra.Config;
 
 public class ServicoJaDbContext(DbContextOptions<ServicoJaDbContext> opt)
     : IdentityDbContext<UsuarioIdentity, IdentityRole<long>, long>(opt)
 {
-    public DbSet<UsuarioIdentity> Usuarios { get; set; } = null!;
+    public DbSet<Perfil> Perfis { get; init; }
+    public DbSet<Servico> Servicos { get; init; }
+    public DbSet<OrdemServico> OrdemServicos { get; init; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
