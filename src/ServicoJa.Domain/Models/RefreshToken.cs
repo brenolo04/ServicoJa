7159da8e4d22084bb5dab1aa368c6aee8f32362b
Breno C. Fonseca <brenocaires04@gmail.com>
2026-05-05 09:57:07 -0300

    feat: implementacao do refresh token na aplicacao

---
+++ src/ServicoJa.Domain/Models/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace ServicoJa.Domain.Models;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; }
+    public long IdUsuarioIdentity { get; set; }
+    public DateTime ExpiresOnUtc { get; set; }
+}
---
+++ src/ServicoJa.Infra/Config/Mappings/RefreshTokenMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServicoJa.Domain.Models;
+
+namespace ServicoJa.Infra.Config.Mappings;
+
+public class RefreshTokenMap : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.ToTable("RefreshToken")
+            .HasKey(r => r.Id);
+
+        builder.Property(r => r.Token)
+            .HasMaxLength(200);
+
+        builder.HasIndex(r => r.Token)
+            .IsUnique();
+
+        builder.Property(r => r.ExpiresOnUtc)
+            .HasColumnType("timestamp without time zone");
+    }
+}
